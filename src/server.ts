10f2e56b583ad546c00bf94c91f378691c962fbd
developersajeeb <sajeebdeveloper@gmail.com>
2025-07-10 20:21:21 +0600

    project setup

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,64 @@
+/* eslint-disable no-console */
 import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
+import { envVars } from './app/config/env';
 
 let server: Server;
 
-const PORT = 5000;
-
-async function main() {
+async function StartServer() {
     try {
-        await mongoose.connect("mongodb+srv://triplanbysajeeb:LbKKqc8mjjBpPFJ3@cluster0.d9amltv.mongodb.net/triplan?retryWrites=true&w=majority&appName=Cluster0");
+        console.log(envVars.NODE_ENV);
+        
+        await mongoose.connect(envVars.DB_URL);
         console.log('Server is connected to database!');
-        server = app.listen(PORT, () => {
-            console.log(`App is listening on port ${PORT}`);
+        server = app.listen(envVars.PORT, () => {
+            console.log(`App is listening on port ${envVars.PORT}`);
         });
     } catch (error) {
         console.log(error);
     }
 }
+StartServer()
 
-main()
+process.on('unhandledRejection', error => {
+    console.log("Unhandled Rejection detected..! Server shutting down.", error);
+    if (server) {
+        server.close(() => {
+            console.log(error);
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+});
+
+process.on('uncaughtException', error => {
+    console.log("uncaughtException exception detected..! Server shutting down.", error);
+    if (server) {
+        server.close(() => {
+            console.log(error);
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+});
+
+process.on('SIGTERM', () => {
+    console.log("SIGTERM is received! Server shutting down.");
+    if (server) {
+        server.close();
+    } else {
+        process.exit(1);
+    }
+});
+
+process.on('SIGINT', () => {
+    console.log("SIGINT is received! Server shutting down.");
+    if (server) {
+        server.close();
+    } else {
+        process.exit(1);
+    }
+});
