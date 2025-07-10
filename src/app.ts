2343e5e5b018330f62b0419adfc5f87ba27b8f6b
developersajeeb <sajeebdeveloper@gmail.com>
2025-07-10 23:28:27 +0600

    user apis and other in progress

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import cors from "cors";
 import express, { Application, Request, Response } from 'express';
+import { router } from "./app/routes";
+
 const app: Application = express();
 app.use(express.json());
+app.use(cors())
 
+app.use("/api/v1", router)
 
 app.get('/', (req: Request, res: Response) => {
     res.send(`
