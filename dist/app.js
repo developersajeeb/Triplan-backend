8b7894cbabaf35c988f8fcbb8875c593ca482ae9
Sajeeb Debnath <93345499+developersajeeb@users.noreply.github.com>
2025-12-09 18:28:27 +0600

    change url

--- dist/app.js
+++ dist/app.js
@@ -15,7 +15,7 @@ const env_1 = require("./app/config/env");
 require("./app/config/passport");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173"], credentials: true
+    origin: ["https://triplan.developersajeeb.com"], credentials: true
 }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
